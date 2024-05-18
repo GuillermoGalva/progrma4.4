0a95a30d758d2c13a9baa416ea557ccb56f6b910
guill <guill@GUILLERMO-PC>
2024-05-17 21:16:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+        int suma = 0;
+
+        while (contador < 10)
+        {
+            Console.Write($"Ingresa el valor {contador + 1}: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            suma += valor;
+            contador++;
+        }
+
+        double promedio = (double)suma / contador;
+
+        Console.WriteLine($"La suma de los valores ingresados es: {suma}");
+        Console.WriteLine($"El promedio de los valores ingresados es: {promedio}");
+    }
+}
